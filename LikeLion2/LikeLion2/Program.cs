e82c53c0aba447159599867916c27669fd0d4eb1
JS66KIM <junseong6980@gmail.com>
2025-02-21 10:23:03 +0900

    study_01_int

---
+++ LikeLion2/LikeLion2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            age = 26;
+            Console.Write(age);
+        }
+    }
+}
