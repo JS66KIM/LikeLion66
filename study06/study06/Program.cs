852cc8c4a91836c783914e0520965a81d90bd2c1
JS66KIM <junseong6980@gmail.com>
2025-02-24 10:32:59 +0900

    s6

---
+++ study06/study06/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bininput = Console.ReadLine();
+            int dec = Convert.ToInt32(bininput, 2);
+
+                Console.WriteLine($"Hi bro{dec}");
+        }
+    }
+}
